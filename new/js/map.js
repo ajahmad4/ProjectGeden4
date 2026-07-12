9c9ce5b31888f4edf312ed31e7b96706d226a32d
ajahmad4 <atabadrul@gmail.com>
2026-07-12 22:23:40 +0700

    Delete new directory

--- new/js/map.js
+++
@@ -1,9 +0,0 @@
-// js/map.js
-
-// Menginisialisasi peta dan mengatur titik tengah awal
-const map = L.map('map').setView([21.4225, 39.8262], 6);
-
-// Memanggil basemap dari internet
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', { 
-    maxZoom: 18 
-}).addTo(map);
